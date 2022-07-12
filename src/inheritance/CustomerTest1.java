7b13cc5044bd6d12fb55a158f5a78b66dcde3b10
jaamong <amypure225@naver.com>
2022-07-12 15:14:06 +0900

    오버라이딩과 다형성(3)

--- src/inheritance/CustomerTest1.java
+++ src/inheritance/CustomerTest1.java
@@ -2,16 +2,12 @@ package inheritance;
 
 public class CustomerTest1 {
     public static void main(String[] args) {
-//        Customer customer1 = new Customer(10100, "me");
-
-        VIPCustomer customer2 = new VIPCustomer(10101, "meme");
 
         //상위 클래스로의 묵시적 형 변환(업캐스팅)
         //타입은 Customer, 생성은 VIPCustomer -> 생성자는 VIPCustomer가 불렸으므로 메모리는 하위 클래스만 생성
         //하지만 customerTest의 타입이 Customer이므로 Customer 클래스의 멤버 변수, 메서드만 사용가능
-        Customer customerTest = new VIPCustomer(10000,"ma");
+        Customer customerTest = new VIPCustomer(10000,"ma", 100);
 
-//        System.out.println(customer1.showCustomerInfo());
-        System.out.println(customer2.showCustomerInfo());
+        System.out.println(customerTest.showCustomerInfo());
     }
 }
--- src/inheritance/VIPCustomer.java
+++ src/inheritance/VIPCustomer.java
@@ -2,9 +2,9 @@ package inheritance;
 
 public class VIPCustomer extends Customer { //Customer 상속받음
     private int agentID;
-    private double saleRatio;
+    double saleRatio;
 
-    public VIPCustomer(int customerID, String customerName) {
+    public VIPCustomer(int customerID, String customerName, int agentID) {
 
         //precompile 때 하위 클래스에서 super();를 이용하여 상위 클래스의 default constructor를 호출
         //default constructor가 아닌 인자가 있는 생성자인 경우 super를 명시적으로 호출하고 그 안에 인자를 넣어준다.
@@ -13,8 +13,14 @@ public class VIPCustomer extends Customer { //Customer 상속받음
         customerGrade = "VIP";
         bonusRatio = 0.05;
         saleRatio = 0.1;
+        this.agentID = agentID;
 
         System.out.println("VIPCustomer(int, String) 호출");
+    }
+
+    public int calPrice(int price) { //overriding : 상위 클래스 메서드와 똑같은 이름, 똑같은 매개변수의 메서드를 재정의
+        bonusPoint += price * bonusRatio; //일반 고객과 VIP 고객의 할인 비율이 다르므로 override
+        return price - (int) (price * saleRatio);
     }
 
     public int getAgentID() {
